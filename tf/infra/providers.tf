6bbf34cdb02e648793a96038c7fba543f9bf3f55
Shahar Mintz <smintz@packet.net>
2020-03-19 23:37:00 +0200

    test: use gcp

--- tf/infra/providers.tf
+++ tf/infra/providers.tf
@@ -1,11 +1,13 @@
 variable "digitalocean_token" {}
 variable "cloudflare_email" {}
 variable "cloudflare_api_key" {}
+variable "gcp_creds" {}
 
 provider "digitalocean" {
   token = var.digitalocean_token
 }
 provider "google" {
+  credentials = var.gcp_creds
   project = "protoconf"
   region = "eu-central1"
 }
